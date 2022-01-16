620c82830cd6743edd3534d4bd94f835f126f4ee
Harry Wolff <hswolff@gmail.com>
2022-01-16 14:05:21 -0500

    handle more edge cases for computeGuess

--- src/word-utils.test.ts
+++ src/word-utils.test.ts
@@ -41,4 +41,28 @@ describe('computeGuess', () => {
       LetterState.Miss,
     ]);
   });
+
+  test('returns empty array when given incomplete guess', () => {
+    expect(computeGuess('so', 'boost')).toEqual([]);
+  });
+
+  test('when 2 letters are present but answer has only 1 of those letters', () => {
+    expect(computeGuess('allol', 'smelt')).toEqual([
+      LetterState.Miss,
+      LetterState.Present,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ]);
+  });
+
+  test('when 1 letter matches but guess has more of the same letter', () => {
+    expect(computeGuess('allol', 'colon')).toEqual([
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Match,
+      LetterState.Match,
+      LetterState.Miss,
+    ]);
+  });
 });
--- src/word-utils.ts
+++ src/word-utils.ts
@@ -6,25 +6,83 @@ export enum LetterState {
   Match, // Letter exists and is in the right location
 }
 
-export function getRandomWord(): string {
-  return wordBank[Math.floor(Math.random() * wordBank.length)];
-}
-
-export function computeGuess(guess: string, wordString: string): LetterState[] {
+export function computeGuess(
+  guess: string,
+  answerString: string = word
+): LetterState[] {
   const result: LetterState[] = [];
-  const word = wordString.split('');
 
-  guess.split('').forEach((letter, index) => {
-    const currentWordLetter = word[index];
+  if (guess.length !== answerString.length) {
+    return result;
+  }
 
-    if (currentWordLetter === letter) {
+  const answer = answerString.split('');
+
+  const guessAsArray = guess.split('');
+
+  const answerLetterCount: Record<string, number> = {};
+
+  guessAsArray.forEach((letter, index) => {
+    const currentAnswerLetter = answer[index];
+
+    answerLetterCount[currentAnswerLetter] = answerLetterCount[
+      currentAnswerLetter
+    ]
+      ? answerLetterCount[currentAnswerLetter] + 1
+      : 1;
+
+    if (currentAnswerLetter === letter) {
       result.push(LetterState.Match);
-    } else if (word.includes(letter)) {
+    } else if (answer.includes(letter)) {
       result.push(LetterState.Present);
     } else {
       result.push(LetterState.Miss);
     }
   });
 
+  result.forEach((curResult, resultIndex) => {
+    if (curResult !== LetterState.Present) {
+      return;
+    }
+    console.log(answerLetterCount);
+
+    const guessLetter = guessAsArray[resultIndex];
+    console.log({ guessLetter });
+
+    //      50|     expect(computeGuess('alloy', 'smelt')).toEqual([
+
+    answer.forEach((currentAnswerLetter, answerIndex) => {
+      console.log({
+        resultIndex,
+        guessLetter,
+        currentAnswerLetter,
+        resultAnswerIndex: result[answerIndex],
+        answerIndex,
+        answerLetterCount: answerLetterCount[guessLetter],
+      });
+
+      if (currentAnswerLetter !== guessLetter) {
+        return;
+      }
+
+      if (result[answerIndex] === LetterState.Match) {
+        result[resultIndex] = LetterState.Miss;
+      }
+
+      if (answerLetterCount[guessLetter] <= 0) {
+        result[resultIndex] = LetterState.Miss;
+      }
+    });
+
+    answerLetterCount[guessLetter]--;
+    console.log('end loop');
+  });
+
   return result;
 }
+
+function getRandomWord(): string {
+  return wordBank[Math.floor(Math.random() * wordBank.length)];
+}
+
+export const word = getRandomWord();
