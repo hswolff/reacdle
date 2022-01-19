eb85611e58af0e3b241aabfa339ccda0e933f262
Harry Wolff <hswolff@gmail.com>
2022-01-18 22:26:56 -0500

    improve game over modal layout

--- src/App.tsx
+++ src/App.tsx
@@ -73,10 +73,15 @@ export default function App() {
       {isGameOver && (
         <div
           role="modal"
-          className="absolute bg-white border border-gray-500 rounded text-center w-3/4 h-1/2 p-6 left-0 right-0 mx-auto top-1/4"
+          className="absolute bg-white border border-gray-500 rounded text-center
+            w-11/12 h-1/2 p-6 left-0 right-0 mx-auto top-1/4
+           grid grid-rows-4"
         >
           <p>Game Over</p>
-          <WordRow word={state.answer} />
+          <WordRow
+            word={state.answer}
+            className="items-center justify-items-center"
+          />
 
           <button
             className="border border-green-500 rounded bg-green-500 p-2 mt-4 text-gray-800 shadow"
